14aed77f5c997865cf13ef3a47eb59fd1c3fa68c
Soham Sachin Pirale <162886548+sohampirale@users.noreply.github.com>
2025-01-23 05:46:43 +0000

    Leanred About Scanners

---
+++ Scanner/ScannerLearning.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class ScannerLearning{
+    public static int getChoice(){
+        Scanner makeChoice=new Scanner(System.in);
+        int choice;
+        System.out.println("1 : Input an Integer");
+        System.out.println("2 : Input a word");
+        System.out.println("3 : Input a line");
+        System.out.println("4 : Input a character");
+        System.out.print("Your choice : ");
+        choice=makeChoice.nextInt();
+        return choice;
+    }
+
+    public static void inputNum(){
+        int num;
+        Scanner numScanner=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        num=numScanner.nextInt();
+        System.out.println("You entered : "+num);
+    }
+
+    public static void inputWord(){
+        String word;
+        Scanner wordScanner=new Scanner(System.in);
+        System.out.print("Enter word : ");
+        word=wordScanner.next();
+        System.out.println("You entered : "+word);
+    }
+
+    public static void inputLine(){
+        String line;
+        Scanner lineScanner=new Scanner(System.in);
+        System.out.print("Enter a line : ");
+        line=lineScanner.nextLine();
+        System.out.println("You entered line : "+line);
+    }
+
+    public static delimiterExample(){
+        string str;
+        Scanner lineScanner=new Scanner(System.in);
+    }
+
+    public static void inputChar(){
+        String ch;
+        Scanner charScanner= new Scanner(System.in);
+        System.out.println("Enter a character : ");
+        ch=charScanner.next();
+        if(ch.length()>1){
+            System.out.println("You enetred a string not character");
+        } else {
+            System.out.println("Charcater entered : "+ch.charAt(0));
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello JAVA.");
+        int choice=1;
+        while(choice!=0){
+            choice=getChoice();
+            if(choice==1){
+                inputNum();
+            } else if(choice==2){
+                inputWord();
+            }else if(choice==3){
+                inputLine();
+            }  else if(choice==4){
+                inputChar();
+            }
+        }
+    }
+}
---
+++ SecondCode.java
@@ -0,0 +1,6 @@
+import FirstCode;
+public class SecondCode{
+    public static void main(String []args){
+        FirstCode.main(args);
+    }
+}
